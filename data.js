f513d8b0da8f7628a268c6fc8d2644db70d2c59e
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2021-12-15 08:00:40 +0100

    new start

--- data.js
+++ data.js
@@ -1,55 +1,45 @@
-// TABS CONTENTS ==================================
-let allTabsContents = [
-  {
-    "tabId" : "Shop",
-    "tabBtnHTML": "<button class='tablinks' onclick='openTabs(event, `Shop`)'>Shop</button>",
-    "tabContentHTML": "<div id='Shop' class='tabcontent'><h3>THIS IS THE SHOP</h3><p>Pleins de truc incroyable ici</p></div>"
-  },
-  {
-    "tabId" : "Crypto",
-    "tabBtnHTML":"<button class='tablinks' onclick='openTabs(event, `Crypto`)'>Crypto</button>",
-    "tabContentHTML":"<div id='Crypto' class='tabcontent'><h3>THIS IS THE CRYPTO</h3><p>Pleins de truc incroyable ici</p></div>",
-  },
-  {
-    "tabId" : "Task",
-    "tabBtnHTML":"<button class='tablinks' onclick='openTabs(event, `Task`)'>Task</button>",
-    "tabContentHTML":"<div id='Task' class='tabcontent'><h3>THIS IS THE TASK</h3><p>Pleins de truc incroyable ici</p></div>",
-  },
-  {
-    "tabId" : "Stats",
-    "tabBtnHTML":"<button class='tablinks' onclick='openTabs(event, `Stats`)'>Stats</button>",
-    "tabContentHTML":"<div id='Stats' class='tabcontent'><h3>THIS IS THE STATS</h3><p>Pleins de truc incroyable ici</p></div>",
-  }
-]
+// ALL DATA THAT SAVE PROGRESS =====================
+let STATES = {
+  "checkpointProgress" : 0,
+  "mainBarProgress" : 0,
+  "clickMultiplicator" : 1,
+  "currentMoney" : 0,
+  "PCPower" : 0,
+  
+  "cryptoAInStock" : 0,
+  "cryptoBInStock" : 0,
+  "cryptoCInStock" : 0,
 
-// CRYPTO INIT DATA ================================
-let initCryptoData = [
+  "isFormularEnable": false,
+  "isShopEnable" : false,
+  "isCryptoEnable" : false,
+  "isTaskEnable" : false,
+};
+
+// CRYPTO DATA ================================
+let CryptoData = [
   {
     "cryptoId" : "cryptoA",
     "cryptoName" : "TonyCoin",
     "cryptoMinVal" : 10,
     "cryptoMedVal" : 20,
-    "cryptoMaxVal" : 30
+    "cryptoMaxVal" : 30,
+    "cryptoTimeInterval" : 100
   },
   {
     "cryptoId" : "cryptoB",
     "cryptoName" : "ClickCoin",
     "cryptoMinVal" : 10,
     "cryptoMedVal" : 20,
-    "cryptoMaxVal" : 30
+    "cryptoMaxVal" : 30,
+    "cryptoTimeInterval" : 100
   },
   {
     "cryptoId" : "cryptoC",
     "cryptoName" : "SullyCoin",
     "cryptoMinVal" : 10,
     "cryptoMedVal" : 20,
-    "cryptoMaxVal" : 30
-  },
-  {
-    "cryptoId" : "cryptoD",
-    "cryptoName" : "KekCoin",
-    "cryptoMinVal" : 10,
-    "cryptoMedVal" : 20,
-    "cryptoMaxVal" : 30
+    "cryptoMaxVal" : 30,
+    "cryptoTimeInterval" : 100
   }
 ]
